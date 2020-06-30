bc927b0b05bf92f01bbb14783752dabceab9c25d
Harry <eriklive2@gmail.com>
2020-06-30 15:18:50 -0300

    updates

--- src/app/not-equal-to/directive.ts
+++ src/app/not-equal-to/directive.ts
@@ -1,28 +1,34 @@
-import { Directive, Input, forwardRef, OnInit } from '@angular/core';
-import { NG_VALIDATORS, Validator, FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Directive, Input, forwardRef, OnInit } from "@angular/core";
+import {
+    NG_VALIDATORS,
+    Validator,
+    FormControl,
+    ValidatorFn,
+} from "@angular/forms";
 
-import { notEqualTo } from './validator';
+import { notEqualTo } from "./validator";
 
 const NOT_EQUAL_TO_VALIDATOR: any = {
-  provide: NG_VALIDATORS,
-  useExisting: forwardRef(() => NotEqualToValidator),
-  multi: true
+    provide: NG_VALIDATORS,
+    useExisting: forwardRef(() => NotEqualToValidator),
+    multi: true,
 };
 
 @Directive({
-  selector: '[notEqualTo][formControlName],[notEqualTo][formControl],[notEqualTo][ngModel]',
-  providers: [NOT_EQUAL_TO_VALIDATOR]
+    selector:
+        "[notEqualTo][formControlName],[notEqualTo][formControl],[notEqualTo][ngModel]",
+    providers: [NOT_EQUAL_TO_VALIDATOR],
 })
 export class NotEqualToValidator implements Validator, OnInit {
-  @Input() notEqualTo: FormControl;
+    @Input() notEqualTo: FormControl;
 
-  private validator: ValidatorFn;
+    private validator: ValidatorFn;
 
-  ngOnInit() {
-    this.validator = notEqualTo(this.notEqualTo);
-  }
+    ngOnInit() {
+        this.validator = notEqualTo(this.notEqualTo);
+    }
 
-  validate(c: AbstractControl): {[key: string]: any} {
-    return this.validator(c);
-  }
+    validate(c: FormControl): { [key: string]: any } {
+        return this.validator(c);
+    }
 }
